3501df53c087c0090fc3cc604ecacd88394958c7
Kubavat Janki <jankik337@gmail.com>
2026-02-25 13:11:07 +0530

    Enhance model schema with timestamps
    Added timestamps option to model schema.

--- models/Model.js
+++ models/Model.js
@@ -1,13 +1,16 @@
 const mongoose = require("mongoose");
 
-const modelSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  image: { type: String },
-  brandId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Brand",
-    required: true
-  }
-});
+const modelSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    image: String,
+    brandId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+      required: true
+    }
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Model", modelSchema);
