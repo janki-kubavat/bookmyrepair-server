868ef20b8b2100d65500ca278cd7d5d43b6f56df
Kubavat Janki <jankik337@gmail.com>
2026-02-26 13:40:35 +0530

    Add URL-encoded data parsing middleware

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true }));
 
 /* ================= DATABASE ================= */
 
