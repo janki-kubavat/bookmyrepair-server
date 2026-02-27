ba598f67aeef18739832bc2ac7b63257fa2ccb89
Kubavat Janki <jankik337@gmail.com>
2026-02-27 11:42:08 +0530

    Add service schema for mongoose model

--- models/Service.js
+++ models/Service.js
@@ -1,1 +1,38 @@
 
+const mongoose = require("mongoose");
+
+const serviceSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: String,
+
+    icon: String, // optional icon name
+    image: String, // uploaded image
+
+    price: {
+      type: Number,
+      required: true,
+    },
+
+    brand: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Brand",
+    },
+
+    model: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Model",
+    },
+
+    isActive: {
+      type: Boolean,
+      default: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Service", serviceSchema);
