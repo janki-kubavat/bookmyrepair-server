2a14004bc399382259088ceb8df79b0b6692ce62
Kubavat Janki <jankik337@gmail.com>
2026-02-27 10:30:43 +0530

    Configure CORS middleware with specific options

--- server.js
+++ server.js
@@ -17,9 +17,17 @@ const Technician = require("./models/Technician");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 /* ================= MIDDLEWARE ================= */
 
-app.use(cors());
+app.use(cors({
+  origin: "https://bookmyrepair.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type"],
+}));
+
+app.options("*", cors());
+
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 
