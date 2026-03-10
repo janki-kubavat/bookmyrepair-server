7071e23e38d94529913da963fc2dd559ead33f23
Kubavat Janki <jankik337@gmail.com>
2026-03-10 15:02:49 +0530

    Update Service.js

--- models/Service.js
+++ models/Service.js
@@ -1,15 +1,22 @@
 const mongoose = require("mongoose");
 
-const serviceSchema = new mongoose.Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-      trim: true,
-      unique: true,
-    }
-  },
-  { timestamps: true }
-);
+const ServiceSchema = new mongoose.Schema({
 
-module.exports = mongoose.model("Service", serviceSchema);
+  image: {
+    type: String
+  },
+
+  name: {
+    type: String,
+    required: true
+  },
+
+  subtitle: {
+    type: String
+  }
+
+},{
+  timestamps: true
+});
+
+module.exports = mongoose.model("Service", ServiceSchema);
